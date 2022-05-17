9f102faa74d282bf7ae9510443584270669cb30b
Daniel <dkaczmarek38@gmail.com>
2022-05-17 23:21:54 +0200

    providers setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import theme from "./providers/themeProvider/theme";
 import styled from "styled-components";
-
+import { ThemeProvider } from "styled-components";
 import AddNewItem from "./pages/AddNewItem/AddNewItem";
 import ShowAllItems from "./pages/ShowAllItems/ShowAllItems";
 import Navigation from "./features/Navigation/Navigation";
 
+import DataProvider from "./providers/dataProvider/dataProvider";
+
 const Wrapper = styled.div`
-  background-color: #282c34;
+  background-color: ${(props) => props.theme.colors.backgroundDefault};
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -29,20 +31,24 @@ const Content = styled.div`
 
 const App = () => {
   return (
-    <Wrapper>
-      <Navigation />
-      <Content>
-        <Routes>
-          <Route path="/" exact element={<h1>landing</h1>} />
-          <Route path="add-item" exact element={<AddNewItem />} />
-          <Route path="all-items" exact element={<ShowAllItems />} />
+    <ThemeProvider theme={theme}>
+      <DataProvider>
+        <Wrapper>
+          <Navigation />
+          <Content>
+            <Routes>
+              <Route path="/" exact element={<h1>landing</h1>} />
+              <Route path="add-item" exact element={<AddNewItem />} />
+              <Route path="all-items" exact element={<ShowAllItems />} />
 
-          <Route path="item/:id" exact element={<ShowAllItems />} />
+              <Route path="item/:id" exact element={<ShowAllItems />} />
 
-          <Route path="*" element={<h1>404</h1>} />
-        </Routes>
-      </Content>
-    </Wrapper>
+              <Route path="*" element={<h1>404</h1>} />
+            </Routes>
+          </Content>
+        </Wrapper>
+      </DataProvider>
+    </ThemeProvider>
   );
 };
 
---
+++ src/providers/themeProvider/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    backgroundDefault: "#282c34",
+    success: "mediumseagreen",
+  },
+};
+
+export default theme;
